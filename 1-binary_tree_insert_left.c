965f21c2b9b51afc2280839895682526f494ceb9
mmanumos <mosqueramanuel5@gmail.com>
2020-03-11 19:56:03 +0000

    [Fix] A validation is removed. modified:   1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -19,13 +19,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 	}
 
-	if (node == NULL)
-		return (NULL);
-
 	if (parent->left == NULL)
 	{
 		parent->left = node;
-		node->left = NULL;
+		return (node);
 	}
 	else
 	{
