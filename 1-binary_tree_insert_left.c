9c12a4a1d079ac93ecb11b06c34fd4455cf8d0a7
mmanumos <mosqueramanuel5@gmail.com>
2020-03-11 19:05:22 +0000

    [Feature] Insert new node to the side left. new file:   1-binary_tree_insert_left.c modified:   binary_trees.h

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - Entry point
+ *
+ * @parent: parent node.
+ * @value: value -> n
+ * Return: Node
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+
+	if (node == NULL)
+		return (NULL);
+
+	node->parent = parent;
+	node->n = value;
+	node->right = NULL;
+
+	if (parent->left == NULL)
+	{
+		parent->left = node;
+		node->left = NULL;
+	}
+	else
+	{
+		node->left = parent->left;
+		parent->left->parent = node;
+		parent->left = node;
+	}
+	return (node);
+}
