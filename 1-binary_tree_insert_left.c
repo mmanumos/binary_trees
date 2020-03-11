20e6dd0b76503a7258b16c6ae91b94adb78051a3
mmanumos <mosqueramanuel5@gmail.com>
2020-03-11 20:02:49 +0000

    [Fix] Correction again. modified:   1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,34 +1,38 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_insert_left - Entry point
- *
- * @parent: parent node.
- * @value: value -> n
- * Return: Node
+ * binary_tree_insert_left - C function to insert node to left side of tree
+ * @parent: pointer to node addition
+ * @value: value to be added
+ * Return: return pointer to new node added
  */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node;
-
-	node = binary_tree_node(parent, value);
+	binary_tree_t *newNode;
 
 	if (parent == NULL)
-	{
 		return (NULL);
-	}
+
+	newNode  = malloc(sizeof(binary_tree_t));
+
+	if (newNode == NULL)
+		return (NULL);
+
+	newNode->n = value;
+	newNode->parent = parent;
+	newNode->right = NULL;
 
 	if (parent->left == NULL)
 	{
-		parent->left = node;
-		return (node);
+		parent->left = newNode;
+		newNode->left = NULL;
 	}
 	else
 	{
-		node->left = parent->left;
-		parent->left->parent = node;
-		parent->left = node;
+		newNode->left = parent->left;
+		parent->left->parent = newNode;
+		parent->left = newNode;
 	}
-	return (node);
+	return (newNode);
+
 }
